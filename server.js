0172527453217fb02ff1fb056e77bf80153ab580
Akram  Yakubov <akram.yakubov@gmail.com>
2023-04-18 12:14:19 -0700

    Update logging and make port configurable

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Mali = require('mali')
 const hl = require('highland')
 
 const PROTO_PATH = path.resolve(__dirname, './protos/echo.proto')
-const HOSTPORT = '0.0.0.0:9090'
+const PORT = process.env.PORT | '8080';
 
 const streamData = [{
   message: 'Hello Bob'
@@ -26,21 +26,21 @@ const streamData = [{
  */
 async function echoUnary (ctx) {
   console.dir(ctx.metadata, { depth: 3, colors: true })
-  console.log(`got sayHello request name: ${ctx.req.message}`)
-  ctx.res = { message: 'Hello ' + ctx.req.message }
-  console.log(`set sayHello response: ${ctx.res.message}`)
+  console.log(`got echoUnary request message: ${ctx.req.message}`)
+  ctx.res = { message: 'Echo ' + ctx.req.message }
+  console.log(`set echoUnary response: ${ctx.res.message}`)
 }
 
 async function echoServerStream (ctx) {
   console.dir(ctx.metadata, { depth: 3, colors: true })
-  console.log(`got sayHellos request name: ${ctx.req.message}`)
+  console.log(`got echoServerStream request message: ${ctx.req.message}`)
   ctx.res = hl(streamData)
   console.log(`done sayHellos`)
 }
 
 async function echoClientStream (ctx) {
   console.dir(ctx.metadata, { depth: 3, colors: true })
-  console.log('got sayHelloCs')
+  console.log('got echoClientStream')
   let counter = 0
   return new Promise((resolve, reject) => {
     hl(ctx.req)
@@ -54,7 +54,7 @@ async function echoClientStream (ctx) {
         .collect()
         .toCallback((err, result) => {
           if (err) return reject(err)
-          console.log(`done sayHelloCs counter ${counter}`)
+          console.log(`done echoClientStream counter ${counter}`)
           ctx.response.res = { message: 'Hello ' + counter }
           resolve()
         })
@@ -62,7 +62,7 @@ async function echoClientStream (ctx) {
 }
 
 async function echoStream (ctx) {
-  console.log('got sayHelloBidi')
+  console.log('got echoStream')
   console.dir(ctx.metadata, { depth: 3, colors: true })
   let counter = 0
   ctx.req.on('data', d => {
@@ -70,7 +70,7 @@ async function echoStream (ctx) {
     ctx.res.write({ message: 'Hello ' + d.message })
   })
   ctx.req.on('end', () => {
-    console.log(`done sayHelloBidi counter ${counter}`)
+    console.log(`done echoStream counter ${counter}`)
     ctx.res.end()
   })
 }
@@ -82,8 +82,8 @@ async function echoStream (ctx) {
 function main () {
   const app = new Mali(PROTO_PATH, 'EchoService')
   app.use({ echoUnary, echoServerStream, echoClientStream, echoStream })
-  app.start(HOSTPORT)
-  console.log(`Echo service running @ ${HOSTPORT}`)
+  app.start(`0.0.0.0:${PORT}`)
+  console.log(`Echo service running @ :${PORT}`)
 }
 
 main()
