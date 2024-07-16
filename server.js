a86f936b75ee8fb18921b3b41076e14be33fd6e5
Ankit Kumar <ankitjames25@gmail.com>
2024-07-16 14:03:37 +0530

    Metadata change

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const streamData = [{
 async function echoUnary (ctx) {
   console.dir(ctx.metadata, { depth: 3, colors: true })
   console.log(`got echoUnary request message: ${ctx.req.message}`)
+  ctx.set('dummyKey', 'dummyValue')
   ctx.res = { message: ctx.req.message }
   console.log(`set echoUnary response: ${ctx.res.message}`)
 }
